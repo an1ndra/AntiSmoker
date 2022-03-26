3e81801e0e4d8551b48e115f739d754e90d7f2f1
Anindra <anindrakarmakar@protonmail.com>
2022-03-26 16:01:53 +0530

    add some data input options

--- app/src/main/java/com/codeavenge/antismoker/MainActivity.java
+++ app/src/main/java/com/codeavenge/antismoker/MainActivity.java
@@ -3,6 +3,7 @@ package com.codeavenge.antismoker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); //hide the title bar
     }
 }
